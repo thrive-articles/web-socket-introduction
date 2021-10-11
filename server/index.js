3043be773528c1f0e63bb91132baa0f5ce4b8ebf
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-10-11 09:30:09 +0530

    Finished

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const server = new webSocket.Server({port: '3000'});
 
 server.on('connection', socket => {
     socket.on('message', message => {
-        socket.send(`I recieved ${message}`);
+        socket.send(`Hello ${message}`);
     })
 })
