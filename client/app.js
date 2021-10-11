34957d131445960e6c99c34f4f7a8e3ec0354219
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-10-11 09:30:40 +0530

    Finished

--- client/app.js
+++ client/app.js
@@ -2,6 +2,7 @@ const socket = new WebSocket('ws://localhost:3000');
 socket.onmessage = ({data}) => {
     console.log(`Message from server ${data}`);
 }
-document.querySelector('button').onclick = () => {
-    socket.send('hello');
+document.querySelector('button').onclick = (bt) => {
+    const msg = document.querySelector('input');
+    socket.send(msg.value)
 }
